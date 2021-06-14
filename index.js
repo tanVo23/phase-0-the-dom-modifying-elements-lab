875360b2198e47e89655d1dbb6f43a63aa2d89c2
Tan <tan10tvo@gmail.com>
2021-06-14 11:34:12 -0600

    commit tree a1989d0bba942ffe03313b205e5a5f8663194fc1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.body.appendChild(element); 
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul); 
